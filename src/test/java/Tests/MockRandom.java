7a5d0ee06d4ce82ef4cc5338d289f226f1568c94
AgileSprinter1 <agiftmacher3d@gmail.com>
2013-02-01 11:18:32 -0800

    Attempt 8: Removing UTF-8 BOM from all java files

--- src/test/java/Tests/MockRandom.java
+++ src/test/java/Tests/MockRandom.java
@@ -1,13 +1,1 @@
-﻿package Tests;
-
-import java.util.Random;
-
-public class MockRandom extends Random {
-
-	private static final long serialVersionUID = 232589230472485106L;
-
-	public int nextInt(int maxValue) {
-		// always return 1/2 max: It isn't random, and that's the point!
-		return maxValue / 2;
-	}
-}
+package Tests;import java.util.Random;public class MockRandom extends Random {	private static final long serialVersionUID = 232589230472485106L;	public int nextInt(int maxValue) {		// always return 1/2 max: It isn't random, and that's the point!		return maxValue / 2;	}}
