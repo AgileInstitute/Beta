e11d48bfb2a5f383f8016a2ff0e0e9e5089d7518
snakeiis <steve.n.kappel@us.hsbc.com>
2013-09-04 16:32:38 -0500

    Update Book.java

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -2,6 +2,58 @@
  * 
  */
 package main.java;
+/**
+ * 
+ */
+package main.java;
+
+
+/**
+ * @author emp197
+ *
+ */
+public class Book {
+
+	private String title;
+	private Edition edition;
+
+	public Book(String title, Edition edition) {
+		this.title = title;
+		this.edition = edition;
+	}
+
+	public int getDiscount(User loggedInUser) {
+		if (loggedInUser != null){
+		
+			if (loggedInUser.hasPurchasedBook(this)){
+				System.out.println(this.edition);
+				if (loggedInUser.getPurchasedBook(this).isSameEdition(Edition.KINDLE))
+					return 10;
+			}
+			System.out.println(this.title);
+			System.out.println(loggedInUser.getPurchasedBook(this));
+			return 10;
+		}
+			
+		return 0;
+	}
+
+	public boolean isSameTitleAs(Book bookToCheck) {
+		return this.title.equals(bookToCheck.title);
+	}
+
+	public Edition edition() {
+		return edition;
+	}
+
+	public boolean isSameEdition(Edition edition) {
+        if (this.edition.equals(edition))
+        	return true;
+        
+		return false;
+	}
+
+}
 
 
 /**
