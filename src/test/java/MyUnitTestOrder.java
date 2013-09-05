ea08795f84d68cac83cd440948d25e389e1ec869
snakeiis <steve.n.kappel@us.hsbc.com>
2013-09-05 15:59:05 -0500

    Update MyUnitTestOrder.java

--- src/test/java/MyUnitTestOrder.java
+++ src/test/java/MyUnitTestOrder.java
@@ -20,7 +20,33 @@ public class MyUnitTestOrder {
 		Order  order = new Order ();
 		order.addBook(new Book("Life", 9.95));
 
-		
-		Assert.assertEquals("Title Price Life 9.95", order.displayList());
+		System.out.println(order.displayList());
+		Assert.assertEquals("Title Price Life 9.95 ", order.displayList());
 	}	
+	@Test
+	public void testaddDiscount(){
+		Double	total = 73.85;
+		Order  order = new Order ();
+		order.addBook(new Book("Life", 28.95));
+		order.addBook(new Book("Death", 24.95));
+		order.addBook(new Book("Ying", 9.95));
+		order.addBook(new Book("Yang", 19.95));
+		order.addDiscount("4th free", 9.95);
+		Assert.assertEquals(total, order.getSubTotal());
+
+	}
+	
+	@Test
+	public void testDisplayListWdiscount()
+	{
+	
+		Order  order = new Order ();
+		order.addBook(new Book("Life", 9.95));
+		order.addBook(new Book("Death", 9.95));
+		order.addBook(new Book("Ying", 9.95));
+		order.addBook(new Book("Yang", 9.95));
+		order.addDiscount("4th free", 9.95);
+		Assert.assertEquals("Title Price Life 9.95 Death 9.95 Ying 9.95 Yang 9.95 4th free -9.95", order.displayList());
+	}	
+	
 }
