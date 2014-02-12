5ca961f0708d78eba3a55e740af0afbf3f37f76b
Michael Lepine <mlepine@agilevelocity.com>
2014-02-12 10:31:11 -0600

    Added minimum amount of logic to Auction class to pass scenario being implemented/tested - prevent seller from bidding up own item.

--- src/main/java/Auction.java
+++ src/main/java/Auction.java
@@ -3,27 +3,30 @@ package main.java;
 /**
  * A class that simulates an Auction
  *
- * @author dcroley
- *
  */
 public class Auction {
 
-    /**
-     * The Auction owner name
-     */
-    private String owner = "";
-
-    public Auction(String userName) {
-        owner = userName;
-    }
-
+	private String seller;
+	
+	public Auction(String seller) {
+		
+		this.seller = seller;
+		
+	}
+	
+	public boolean isValidBidder(String bidder) {
+		
+		return !seller.equals(bidder);
+		
+	}
+	
     /**
      * Return whether the given user can modify this instance of an Auction
      * @param userName
      * @return
      */
     public boolean canModify(String userName) {
-        return userName.equals(owner);
+        return userName.equals(seller);
     }
 
     public boolean canBid() {
--- src/test/java/AuctionTest.java
+++ src/test/java/AuctionTest.java
@@ -7,6 +7,33 @@ import org.junit.Test;
 
 public class AuctionTest {
 
+	@Test
+	public void canSellerBidOnOwnAuction() {
+		
+		String seller = "sellerx";
+		Auction auction = new Auction(seller);
+		boolean canBid;
+		
+		canBid = auction.isValidBidder(seller);
+		
+		Assert.assertFalse(canBid);
+		
+	}
+	
+	@Test
+	public void canBidOnAuction() {
+		
+		String seller = "sellerx";
+		String bidder = "bidderx";
+		Auction auction = new Auction(seller);
+		boolean canBid;
+		
+		canBid = auction.isValidBidder(bidder);
+		
+		Assert.assertTrue(canBid);
+		
+	}
+	
     @Test
     public void checkAuctionUpdatePermissions() {
         Auction auction = new Auction("sellerDude");
@@ -19,4 +46,5 @@ public class AuctionTest {
         Assert.assertTrue(auction.canModify("sellerDude"));
        
     }
+		
 }
