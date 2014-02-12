c01eb3ad313f134f36fe70a10f15f37672be65cc
Rob Biedenharn <Rob@AgileConsultingLLC.com>
2014-02-12 15:26:43 -0500

    minify one-liners

--- src/main/java/Auction.java
+++ src/main/java/Auction.java
@@ -13,26 +13,18 @@ public class Auction {
     private int quantity = 0;
     private int minBid;
 
-    public Auction(String seller) {
-        this.seller = seller;
-    }
+    public Auction(String seller) {this.seller = seller;}
 
-    public boolean isValidBidder(String bidder) {
-        return !seller.equals(bidder);
-    }
+    public boolean isValidBidder(String bidder) {return !seller.equals(bidder);}
 
     /**
      * Return whether the given user can modify this instance of an Auction
      * @param userName
      * @return
      */
-    public boolean canModify(String userName) {
-        return userName.equals(seller);
-    }
+    public boolean canModify(String userName) {return userName.equals(seller);}
 
-    public boolean canBid() {
-        return this.isOpen();
-    }
+    public boolean canBid() {return this.isOpen();}
 
     public boolean makeBid(String bidder, int amount) {
         if (isValidBidder(bidder)) {
@@ -43,17 +35,10 @@ public class Auction {
         }
     }
 
-    public int getCurrentBidAmount() {
-        return currentBidAmount;
-    }
-    public void setCurrentBidAmount(int amount) {
-        currentBidAmount = amount;
-    }
+    public int getCurrentBidAmount() {return currentBidAmount;}
+    public void setCurrentBidAmount(int amount) {currentBidAmount = amount;}
 
-    public boolean isOpen() {
-        return open;
-    }
-
+    public boolean isOpen() {return open;}
     public void open() throws AuctionNotReadyException {
         if (getDescription().isEmpty() || getQuantity() == 0) {
             throw new AuctionNotReadyException("Missing fields");
@@ -67,41 +52,27 @@ public class Auction {
      * Closes the auction.
      *
      */
-    public void close() {
-        this.open = false;
-    }
+    public void close() {this.open = false;}
 
-    public String getCondition() {
-        return condition;
-    }
-
+    public String getCondition() {return condition;}
     public void setCondition(String condition) throws AuctionInProgressException {
         if (isOpen()) throw new AuctionInProgressException("Cannot set condition.  Auction is started.");
         this.condition = condition;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
+    public String getDescription() {return description;}
     public void setDescription(String description) throws AuctionInProgressException {
         if (isOpen()) throw new AuctionInProgressException("Cannot set description.  Auction is started.");
         this.description = description;
     }
 
-    public int getQuantity() {
-        return quantity;
-    }
-
+    public int getQuantity() {return quantity;}
     public void setQuantity(int quantity) throws AuctionInProgressException {
         if (isOpen()) throw new AuctionInProgressException("Cannot set quantity.  Auction is started.");
         this.quantity = quantity;
     }
 
-    public int getMinBid() {
-        return minBid;
-    }
-
+    public int getMinBid() {return minBid;}
     public void setMinBid(int minBid) throws AuctionInProgressException {
         if (isOpen()) throw new AuctionInProgressException("Cannot set bid.  Auction is started.");
         this.minBid = minBid;
