3032cd1503bd4fcb80d1d97f82b97855f5175358
David Croley <dcroley@agilevelocity.com>
2014-02-12 10:35:06 -0600

    fix unit test to use constructor with a seller

--- src/test/java/BiddingTest.java
+++ src/test/java/BiddingTest.java
@@ -8,7 +8,7 @@ import main.java.Auction;
 public class BiddingTest {
     @Test
     public void CanBid() {
-        Auction auction = new Auction();
+        Auction auction = new Auction("seller");
         Assert.assertTrue(auction.canBid());
     }
 }
